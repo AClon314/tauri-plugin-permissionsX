7ebee0c8f06af129d744fb6898d9d72a44d330e7
AClon314 <1620340682@qq.com>
2024-04-22 20:57:24 +0800

    Update Tauri API usage and package description

--- guest-js/index.ts
+++ guest-js/index.ts
@@ -1,5 +1,16 @@
-import { invoke } from '@tauri-apps/api/core'
+import { invoke } from "@tauri-apps/api/core";
 
 export async function execute() {
-  await invoke('plugin:permissionsx|execute')
+  await invoke("plugin:permissionsx|execute");
+}
+
+export async function startPersistentNotify(title?: string, content?: string) {
+  return await invoke("plugin:permissionsx|startPersistentNotify", {
+    title: title,
+    content: content,
+  });
+}
+
+export async function stopPersistentNotify() {
+  return await invoke("plugin:permissionsx|stopPersistentNotify");
 }
